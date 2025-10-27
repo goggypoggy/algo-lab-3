bae337fa0160a426bf53bc58d556d1a788c38588
Gosha Pogrebitskiy <goshik200712@gmail.com>
2025-10-27 19:32:29 +0300

    task c ver 1, maxheap only, failed due to being slow

--- c.cpp
+++ c.cpp
@@ -1,5 +1,14 @@
-#include <iostream>
+#include <cassert>
 #include <cstring>
+#include <iostream>
+
+const int kMaxHeap = 100'000;
+
+void Output(int* mas, int size) {
+    for (int i = 0; i < size; ++i) {
+        std::cout << mas[i] << (i == size - 1 ? "\n" : " ");
+    }
+}
 
 void Swap(int& a, int& b) {
     int temp = a;
@@ -32,23 +41,84 @@ void SiftDown(int* heap, int size,  int element) {
     SiftDown(heap, size, j);
 }
 
-void BuildHeap(int* heap, int size) {
-    for (int i = (size / 2) - 1; i >= 0; --i) {
-        SiftDown(heap, size, i);
+int SiftUp(int* heap, int size, int element) {
+    if (element == 0) {
+        return 0;
     }
+
+    int parent = (element - 1) / 2;
+    if (heap[parent] < heap[element]) {
+        Swap(heap[parent], heap[element]);
+        return SiftUp(heap, size, parent);
+    }
+
+    return element;
+}
+
+void Insert(int* heap, int &size, int x) {
+    heap[size] = x;
+    size++;
+    int sifted_up_x = SiftUp(heap, size, size - 1);
+    SiftDown(heap, size, sifted_up_x);
+}
+
+int Remove(int* heap, int& size, int i) {
+    assert(i >= 0 && i < size);
+    int removed = heap[i];
+    Swap(heap[i], heap[size - 1]);
+    size--;
+    int sifted_up_i = SiftUp(heap, size, i);
+    SiftDown(heap, size, sifted_up_i);
+    return removed;
+}
+
+int GetMin(int* heap, int& size) {
+    if (size == 1) {
+        return Remove(heap, size, 0);
+    }
+    int min = (size - 2) / 2 + 1;
+
+    for (int i = min + 1; i < size; ++i) {
+        if (heap[i] < heap[min]) {
+            min = i;
+        }
+    }
+
+    return Remove(heap, size, min);
+}
+
+int GetMax(int* heap, int& size) {
+    return Remove(heap, size, 0);
 }
 
 int main(int argc, char** argv) {
     int q;
-    int* heap = new int[100'000];
-
+    int* heap = new int[kMaxHeap];
+    int size = 0;
+    
     std::cin >> q;
 
     for (int i = 0; i < q; ++i) {
         char cmd[7] = {0};
-    }
+        for (int c = 0; c < 6; ++c) {
+            std::cin >> cmd[c];
+        }
 
-    
+        if (strcmp(cmd, "Insert") == 0) {
+            std::cin >> cmd[0]; // skip (
+            int i;
+            std::cin >> i;
+            std::cin >> cmd[0]; // skip )
+
+            Insert(heap, size, i);
+        } else if (strcmp(cmd, "GetMin") == 0) {
+            std::cout << GetMin(heap, size) << std::endl;
+        } else if (strcmp(cmd, "GetMax") == 0) {
+            std::cout << GetMax(heap, size) << std::endl;
+        }
+
+        //Output(heap, size);
+    }
 
     delete[] heap;
     return 0;
